e9c5038f48139bfafccde707431544041d0f2c8d
Jono Gray <jonathan.gray@flowminder.org>
2019-03-11 18:02:05 +0000

    And now with versioning

--- docs/build.sh
+++ docs/build.sh
@@ -9,7 +9,7 @@ set -e
 
 KillJobs() {
     for job in $(jobs -p); do
-    		echo "Killing $job"
+            echo "Killing $job"
             kill -s SIGTERM $job > /dev/null 2>&1 || (sleep 10 && kill -9 $job > /dev/null 2>&1 &)
 
     done
@@ -17,18 +17,18 @@ KillJobs() {
 
 TrapQuit() {
     if [ "$CI" != "true" ]; then
-	    echo "Bringing down containers."
-	    docker-compose -f docs-build-containers.yml down
-	fi
+        echo "Bringing down containers."
+        docker-compose -f docs-build-containers.yml down
+    fi
 
-	echo "Shutting down FlowMachine and FlowAPI"
+    echo "Shutting down FlowMachine and FlowAPI"
     KillJobs
 }
 
 trap TrapQuit EXIT
 
 if [ "$CI" != "true" ]; then
-	export PIPENV_DOTENV_LOCATION=$(pwd)/.env
+    export PIPENV_DOTENV_LOCATION=$(pwd)/.env
     echo "Setting up docker containers"
     echo "Bringing down any existing ones."
     docker-compose -f docs-build-containers.yml down
@@ -37,7 +37,7 @@ if [ "$CI" != "true" ]; then
     echo "Waiting for flowdb to be ready"
     docker exec flowkit_docs_flowdb bash -c 'i=0; until [ $i -ge 24 ] || (pg_isready -h 127.0.0.1 -p 5432); do let i=i+1; echo Waiting 10s; sleep 10; done'
 else
-	export PIPENV_DONT_LOAD_ENV=1
+    export PIPENV_DONT_LOAD_ENV=1
 fi
 
 pushd ../flowmachine
@@ -53,4 +53,4 @@ popd
 echo "Starting build."
 
 pipenv install
-BRANCH=${CIRCLE_BRANCH:="master"} pipenv run mkdocs "${@:-build}"
+GIT_COMMIT=$(git log -1 --format="%H") pipenv run mkdocs "${@:-build}"
